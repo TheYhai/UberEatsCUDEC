654c8aafce6f1225e7e58bf3544209729d122c23
TheYhai <lizcolin39@gmail.com>
2026-08-03 19:50:31 -0600

    Modificacion rapida del codigo

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-self.addEventListener("install", function (event) {
+
+
+self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open("sw-cache").then(function (cache) {
-        return cache.add("index.html");
+    caches.open('sw-cache').then(function (cache) {
+        return cache.add('index.html');
     })
   );
 });
